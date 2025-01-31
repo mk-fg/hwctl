c066469b66306d88eb2509bba4aa0c64c46b8fc6
Mike Kazantsev <mk.fraggod@gmail.com>
2025-01-31 21:05:37 +0500

    nfc-sticker-actions: split nfc reader handling loop into self-subprocess, for better cleanup

--- nfc-sticker-actions.py
+++ nfc-sticker-actions.py
@@ -1,12 +1,87 @@
 #!/usr/bin/env python
 
-import ctypes as ct, collections as cs, pathlib as pl, subprocess as sp
-import os, sys, re, logging, time, signal, errno
-import asyncio, configparser, struct, fcntl, termios
+import os, sys, time, signal, json
 
-# https://github.com/LudovicRousseau/pyscard
-import smartcard.System as sc_sys, smartcard.CardRequest as sc_req
-import threading, smartcard.Exceptions as sc_exc
+class adict(dict):
+	def __init__(self, *args, **kws):
+		super().__init__(*args, **kws)
+		self.__dict__ = self
+
+err_fmt = lambda err: f'[{err.__class__.__name__}] {err}'
+
+
+### Part 1/2 - Subprocess to only interact with nfc-reader and scrap afterwards
+# Using pyscard modules in main process makes it difficult to cleanly
+#  disconnect it from pcsc-lite, stop it easily mid-thread and cleanup other stuff.
+# Hence this stub to run that in a subprocess.
+
+def retries_within_timeout( tries, timeout,
+		backoff_func=lambda e,n: ((e**n-1)/(e*5)), slack=1e-2 ):
+	if tries <= 1 or timeout <= 0: return [0]
+	if tries == 2: return [0, timeout / 2]
+	a, b = 0, timeout
+	while True:
+		m = (a + b) / 2
+		delays = list(backoff_func(m, n) for n in range(tries))
+		error = sum(delays) - timeout
+		if abs(error) < slack: return delays
+		elif error > 0: b = m
+		else: a = m
+
+def main_reader():
+	# https://github.com/LudovicRousseau/pyscard
+	import smartcard.System as sc_sys, smartcard.CardRequest as sc_req
+	import smartcard.Exceptions as sc_exc
+
+	conf = adict(json.loads(sys.stdin.read()))
+	out = lambda s: print(s, flush=True)
+	log_debug, log_err = lambda s: out(f'\0{s}'), lambda s: out(f'\1{s}')
+	ts_bump = lambda *a: conf.update(ts_done=time.monotonic() + conf.td)
+	signal.signal(signal.SIGINT, signal.SIG_DFL)
+	signal.signal(signal.SIGHUP, ts_bump)
+	ts_bump()
+
+	readers_seen = set()
+	for delay in retries_within_timeout(conf.td_warmup_checks, conf.td_warmup):
+		if delay: time.sleep(delay)
+		for rr in (reader := sc_sys.readers()):
+			if (rn := rr.name) in readers_seen: continue
+			log_debug(f'Detected reader [ {rn} ]'); readers_seen.add(rn)
+		if conf.name: reader = list(rr for rr in reader if conf.name in rr.name)
+		if len(reader) == 1: reader = reader[0]; break
+	else:
+		rm = ' matching' if conf.name else ''
+		if not reader: err = f'No{rm} PCSC-lite readers available'
+		elif len(reader) > 1:
+			err = ' '.join(repr(rr.name) for rr in reader)
+			err = f'More than one{rm} PCSC-lite reader: {err}'
+		return log_err(err)
+	log_debug(f'Using reader [ {reader.name} ]')
+	c_new = False
+	while True:
+		td = min(conf.td, conf.ts_done - time.monotonic() + 1)
+		if td < 0: log_debug('reader loop done'); break
+		try:
+			c_req = sc_req.CardRequest(
+				readers=[reader], newcardonly=c_new, timeout=td )
+			c_new, c_svc = True, c_req.waitforcard()
+		except sc_exc.CardRequestTimeoutException: continue
+		try:
+			(conn := c_svc.connection).connect()
+			data, sw1, sw2 = conn.transmit([0xFF, 0xCA, 0x00, 0x00, 0x00])
+			conn.disconnect()
+		except sc_exc.NoCardException: c_new = False # removed too quickly
+		except sc_exc.SmartcardException as err: log_err(f'reader error: {err_fmt(err)}')
+		else: out(bytes(data).hex()); ts_bump()
+
+if __name__ == '__main__' and (
+		len(sys.argv) > 1 and sys.argv[1] == '--nfc' ): sys.exit(main_reader())
+
+
+### Part 2/2 - Main asyncio-loop process
+
+import ctypes as ct, collections as cs, pathlib as pl, subprocess as sp
+import re, logging, struct, fcntl, termios, errno, asyncio, configparser
 
 
 _td_days = dict(
@@ -35,26 +110,6 @@ def td_parse(td_str):
 		# [[HH:]MM:]SS where seconds can be fractional
 		return sum(n*float(m) for n,m in zip((3600, 60, 1), td_str.split(':')))
 	raise ValueError(f'Failed to parse time-delta spec: {td_str}')
-
-def retries_within_timeout( tries, timeout,
-		backoff_func=lambda e,n: ((e**n-1)/(e*5)), slack=1e-2 ):
-	if tries <= 1 or timeout <= 0: return [0]
-	if tries == 2: return [0, timeout / 2]
-	a, b = 0, timeout
-	while True:
-		m = (a + b) / 2
-		delays = list(backoff_func(m, n) for n in range(tries))
-		error = sum(delays) - timeout
-		if abs(error) < slack: return delays
-		elif error > 0: b = m
-		else: a = m
-
-class adict(dict):
-	def __init__(self, *args, **kws):
-		super().__init__(*args, **kws)
-		self.__dict__ = self
-
-err_fmt = lambda err: f'[{err.__class__.__name__}] {err}'
 
 
 conf_defaults = adict(
@@ -219,50 +274,6 @@ async def run_log_tail(p, cb, tail_bytes=10*2**10, oserr_delay=0.05):
 		inotify.close()
 
 
-def run_nfc_reader_loop(name, uid_cb, times):
-	'''Intended to be run in its own thread, using call_soon_threadsafe in uid_cb.
-		uid_cb is called with StopIteration or Exception when function terminates.
-		times=adict(td=td, ts_done=ts_mono, ...) intended to be externally-mutable.'''
-	try: _run_nfc_reader_loop(name, uid_cb, times)
-	except Exception as err: uid_cb(err)
-	else: uid_cb(StopIteration)
-
-def _run_nfc_reader_loop(name, uid_cb, times):
-	readers_seen = set()
-	for delay in retries_within_timeout(times.td_warmup_checks, times.td_warmup):
-		if delay: time.sleep(delay)
-		for rr in (reader := sc_sys.readers()):
-			if (rn := rr.name) in readers_seen: continue
-			log.debug('[nfc] Detected reader [ %s ]', rn); readers_seen.add(rn)
-		if name: reader = list(rr for rr in reader if name in rr.name)
-		if len(reader) == 1: reader = reader[0]; break
-	else:
-		rm = ' matching' if name else ''
-		if not reader: err = f'No{rm} PCSC-lite readers available'
-		elif len(reader) > 1:
-			err = ' '.join(repr(rr.name) for rr in reader)
-			err = f'More than one{rm} PCSC-lite reader: {err}'
-		raise LookupError(err)
-	log.debug('[nfc] Using reader [ %s ]', reader.name)
-	c_new = False
-	while True:
-		td = min(times.td, times.ts_done - time.monotonic())
-		if td < 0: log.debug('[nfc] reader loop done'); break
-		try:
-			c_req = sc_req.CardRequest(
-				readers=[reader], newcardonly=c_new, timeout=td )
-			c_new, c_svc = True, c_req.waitforcard()
-		except sc_exc.CardRequestTimeoutException: continue
-		try:
-			(conn := c_svc.connection).connect()
-			data, sw1, sw2 = conn.transmit([0xFF, 0xCA, 0x00, 0x00, 0x00])
-			conn.disconnect()
-		except sc_exc.NoCardException: c_new = False # removed too quickly
-		except sc_exc.SmartcardException as err:
-			log.error('[nfc] reader error: %s', err_fmt(err))
-		else: times.ts_done = time.monotonic() + times.td; uid_cb(bytes(data).hex())
-
-
 async def run_proc(name, cmd, stdin, timeout, kill_delay=2.0):
 	proc, pre = None, f'[action {name}]'
 	try:
@@ -270,22 +281,47 @@ async def run_proc(name, cmd, stdin, timeout, kill_delay=2.0):
 			proc = await asyncio.create_subprocess_exec(
 				*cmd, stdin=sp.PIPE if stdin else None )
 			if stdin: await proc.communicate(stdin.encode())
+	except asyncio.CancelledError: pass
 	except asyncio.TimeoutError: log.debug('%s Process timed out', pre)
 	except Exception as err: log.error('%s Failed with error: %s', pre, err_fmt(err))
 	if not proc: return
 	if (n := proc.returncode) is not None:
 		return log.debug('%s Process exited (code=%s)', pre, n)
 	try:
-		proc.terminate()
-		async with asyncio.timeout(kill_delay): await proc.wait()
-		if proc.returncode is None: proc.kill()
+		try:
+			async with asyncio.timeout(kill_delay): proc.terminate(); await proc.wait()
+		except asyncio.TimeoutError: proc.kill()
 	except OSError: pass
 	log.debug('%s Process terminated', pre)
 
 
+async def run_reader(name, cb, proc_info, times, kill_delay=2.0):
+	proc, pre, log_fns = None, f'[nfc]', dict(enumerate([log.debug, log.error]))
+	try:
+		proc = await asyncio.create_subprocess_exec(
+			sys.executable or 'python', __file__, '--nfc', stdin=sp.PIPE, stdout=sp.PIPE )
+		proc_info.pid = proc.pid
+		proc.stdin.write(json.dumps(dict(name=name, **times)).encode())
+		await proc.stdin.drain(); proc.stdin.close(); await proc.stdin.wait_closed()
+		while (line := await proc.stdout.readline()):
+			if not (log_fn := log_fns.get(line[0])): cb(line.decode().strip())
+			else: log_fn('[nfc] ' + line[1:].decode().strip())
+	except asyncio.CancelledError: pass
+	except Exception as err: log.error('%s subproc failed: %s', pre, err_fmt(err))
+	if not proc: return
+	if (n := proc.returncode) is not None:
+		return log.debug('%s subproc exited (code=%s)', pre, n)
+	try:
+		try:
+			async with asyncio.timeout(kill_delay): proc.terminate(); await proc.wait()
+		except asyncio.TimeoutError: proc.kill()
+	except OSError: pass
+	log.debug('%s subproc terminated', pre)
+
+
 async def run(conf):
 	tasks, evq, loop = set(), asyncio.Queue(), asyncio.get_running_loop()
-	evq_task = exit_task = nfc_thread = None
+	evq_task = exit_task = nfc_task = None
 	task_done = lambda tt: not tt or tt.done()
 	def task_new(**named_task):
 		(name, coro), = named_task.items()
@@ -294,7 +330,7 @@ async def run(conf):
 	def exit_task_bump():
 		nonlocal exit_task
 		if not task_done(exit_task): exit_task.cancel(); exit_task = None
-		if nfc_thread and nfc_thread.is_alive(): return
+		if not task_done(nfc_task): return
 		if any(tt.get_name() in ['act', 'fifo'] for tt in tasks): return
 		if (td := conf.main.exit_timeout) > 0:
 			log.debug('exit: scheduled in %ss', td)
@@ -327,28 +363,27 @@ async def run(conf):
 		except asyncio.TimeoutError: _log_err('write timeout')
 		finally: os.close(fd)
 
-	nfc_times = adict(
-		td=conf.main.reader_timeout,
-		td_warmup=conf.main.reader_warmup,
-		td_warmup_checks=conf.main.reader_warmup_checks )
+	nfc_proc = adict()
 	def nfc_start():
-		# asyncio.to_thread doesn't work here - doesn't allow to set daemon=True
-		nonlocal nfc_times, nfc_thread, exit_task
-		nfc_times.ts_done = time.monotonic() + nfc_times.td
-		if not task_done(exit_task): exit_task.cancel(); exit_task = None
-		if nfc_thread and nfc_thread.is_alive(): return
-		cb = lambda ev: loop.call_soon_threadsafe(evq.put_nowait, dict(nfc=ev))
-		nfc_thread = threading.Thread( name='nfc_reader_loop', daemon=True,
-			target=run_nfc_reader_loop, args=(conf.main.reader_name, cb, nfc_times) )
-		log.debug('NFC: started reader loop'); nfc_thread.start()
+		nonlocal nfc_task
+		if not task_done(nfc_task) and (pid := nfc_proc.get('pid')):
+			try: return os.kill(pid, signal.SIGHUP)
+			except OSError: return
+		exit_task_bump()
+		task_new(reader=run_reader(
+			conf.main.reader_name, lambda ev: evq.put_nowait(dict(nfc=ev)),
+			nfc_proc, times=adict( td=conf.main.reader_timeout,
+				td_warmup=conf.main.reader_warmup,
+				td_warmup_checks=conf.main.reader_warmup_checks )))
+		log.debug('NFC: started reader subprocess')
 
 	for sig in 'INT', 'TERM': loop.add_signal_handler(
 		getattr(signal, f'SIG{sig}'), lambda _sig=sig: (
 			log.debug('Exiting on signal %s', _sig), evq.put_nowait(None) ) )
 
 	if conf.hwctl.log_file:
-		cb = lambda ev: evq.put_nowait(dict(btn=ev))
-		task_new(btns_log=run_log_tail( pl.Path(conf.hwctl.log_file), cb,
+		task_new(btns_log=run_log_tail(
+			pl.Path(conf.hwctl.log_file), lambda ev: evq.put_nowait(dict(btn=ev)),
 			tail_bytes=conf.hwctl.log_tail_bytes, oserr_delay=conf.hwctl.log_oserr_retry ))
 		exit_task_bump()
 	else: nfc_start()
@@ -364,6 +399,7 @@ async def run(conf):
 			except asyncio.CancelledError: continue
 			log.debug('Loop: %s/%s %s', task, len(tasks), ev or '-')
 			if task == 'exit': ev = None
+			elif task == 'reader': ev = dict(nfc=StopIteration)
 			elif task in ['act', 'fifo']: exit_task_bump(); continue
 			elif task != 'ev': raise RuntimeError(f'Daemon task failed: {task}')
 			if not ev: running = log.debug('Loop: exiting'); continue
@@ -373,25 +409,22 @@ async def run(conf):
 					float(btn_ts), int((m := re.fullmatch(r'btn=(\d+)', btn)) and m[1] or 0) )
 				except: log.debug('hwctl: unrecognized btn-log line [ %s ]', line); continue
 				if time.time() - btn_ts > conf.hwctl.log_time_slack: continue
-				if cmds := conf.hwctl.fifo_enable_btns.get(btn):
-					task_new(fifo=fifo_send(conf.hwctl.fifo, cmds))
+				if not (cmds := conf.hwctl.fifo_enable_btns.get(btn)): continue
+				if conf.hwctl.fifo: task_new(fifo=fifo_send(conf.hwctl.fifo, cmds))
 				nfc_start()
 
-			elif nfc_ev := ev.get('nfc'):
-				if nfc_ev is StopIteration: # clean exit
-					if cmds := conf.hwctl.fifo_disable:
+			elif nfc_uid := ev.get('nfc'):
+				if nfc_uid is StopIteration:
+					if cmds := conf.hwctl.fifo and conf.hwctl.fifo_disable:
 						task_new(fifo=fifo_send(conf.hwctl.fifo, cmds))
-				elif isinstance(nfc_ev, Exception):
-					log.error('NFC: reader failure - %s', err_fmt(nfc_ev))
-				else:
-					nfc_uid, nfc_uid_found = nfc_ev.lower(), False
-					for act in conf.actions.values():
-						if act.uid != nfc_uid: continue
-						task_new(act=run_proc( act.name,
-							act.run, act.get('stdin'), act.get('stop_timeout') ))
-						nfc_uid_found = True
-					if not nfc_uid_found: log.debug('NFC: no action for UID - %s', nfc_uid)
-				exit_task_bump()
+					exit_task_bump(); continue
+				nfc_uid, nfc_uid_found = nfc_uid.lower(), False
+				for act in conf.actions.values():
+					if act.uid != nfc_uid: continue
+					task_new(act=run_proc( act.name,
+						act.run, act.get('stdin'), act.get('stop_timeout') ))
+					nfc_uid_found = True
+				if not nfc_uid_found: log.debug('NFC: no action for UID - %s', nfc_uid)
 
 	log.debug('Loop: finished')
 	for tt in tasks:
@@ -425,11 +458,6 @@ def main(args=None):
 	log = logging.getLogger('nsa')
 
 	conf = conf_parse_ini(pl.Path(opts.conf))
-	if conf.hwctl.log_file and not (
-			conf.hwctl.fifo and conf.hwctl.fifo_enable_btns ):
-		parser.error( f'Configuration file [ {opts.conf} ]'
-			' has hwctl log-file set, but no fifo/actions to run for its lines' )
-
 	try: return asyncio.run(run(conf))
 	except asyncio.CancelledError: pass
 
